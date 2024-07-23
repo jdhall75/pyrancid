44fdac467513d9fdf336155cc6aadeb0dbb5627d
Jason Hall <jdhall75@gmail.com>
2024-07-23 11:57:53 -0400

    init and run cmds coming

--- pyrancid/cli/__init__.py
+++ pyrancid/cli/__init__.py
@@ -1,44 +1,92 @@
 from pyrancid.loaders import config_loader
 import click
+from click.termui import Choice
 import os
 import sys
-from ruamel.yaml import YAML
-
-
-@click.group()
-def main():
-    # setup the logger in main with a default logging config
-    pass
+import ruamel.yaml
+import logging
+from typing import Dict
+from typing import Any
 
 
 def _get_config_default():
     # XDG standard for configs in home directory
-    home_config = f"{os.environ.get('HOME')}/.config/pyrancid/prancid.conf"
+    home_config = f"{os.environ.get('HOME')}/.config/pyrancid/pyrancid.yaml"
     # Global etc location
-    global_config = '/etc/pyrancid/pyrancid.conf'
+    global_config = '/etc/pyrancid/pyrancid.yaml'
 
-    return home_config, global_config
+    return global_config, home_config
 
 
-@click.command()
-@click.option('--config', multiple=True, default=_get_config_default)
+def _query_group_data(name: str):
+    connection_type_choices = Choice(('SSH', 'Telnet'))
+    group_data: Dict[str, Any] = {}
+    group_name: str = click.prompt("Group name", type=str, default=name)
+    auth_user: str = click.prompt("Group auth user", type=str)
+    auth_pass: str = click.prompt(
+        "Group auth password", hide_input=True, type=str)
+    connection_type: str = click.prompt(
+        "Group connection type",
+        type=connection_type_choices,
+        show_choices=True
+    )
+
+    group_data.update({
+        'group_name': group_name.lower(),
+        'group_auth_user': auth_user,
+        'group_auth_pass': auth_pass,
+        'connection_type': connection_type,
+    })
+
+    return group_data
+
+
+CONFIG: Dict[str, Any] = {}
+
+
+@click.group('main')
+@click.option('-c', '--configs', multiple=True, default=_get_config_default)
+def main(configs):
+    yml = ruamel.yaml.YAML()
+    # setup the logger in main with a default logging config
+    if not configs:
+        configs = _get_config_default()
+
+    config_dict: Dict = {}
+    for config in configs:
+        try:
+            with open(config, 'rt') as fp:
+                config_dict.update(yml.load(fp.read()))
+        except FileNotFoundError as e:
+            print(e)
+
+    CONFIG.update(config_dict)
+
+
+@main.command()
 @click.option('-v', '--verbose', count=True)
-def checkconfig(config, verbose):
-    loadable_configs = []
-    for c in config:
-        if not os.path.exists(c):
-            print(c, "doesnt exist")
-        loadable_configs.append(c)
-
-    # return config
-    config = config_loader(loadable_configs)
-    if isinstance(config, dict):
+def checkconfig(verbose):
+    if isinstance(CONFIG, dict):
         if verbose >= 2:
-            print(YAML().dump(config, sys.stdout))
+            print(ruamel.yaml.YAML().dump(CONFIG, sys.stdout))
         print('Config is good')
 
 
-main.add_command(checkconfig)
+@main.command()
+@click.option('-g', '--group')
+def run(group: str = ""):
+    pass
+
+
+@main.command()
+@click.argument('name', required=True)
+def create_group(name: str = None):
+    group_data: Dict[str, Any] = {}
+    if name:
+        group_data['group_name'] = name
+        group_data.update(_query_group_data(name))
+        print(group_data)
+
 
 if __name__ == "__main__":
     main()
